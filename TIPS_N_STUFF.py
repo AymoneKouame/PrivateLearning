bd99366a94271da5b2b5ef316b79497750442bc3
AymoneKouame <AymoneKouame@users.noreply.github.com>
2018-05-29 00:29:50 -0400

    new file:   TIPS_N_STUFF.py

---
+++ TIPS_N_STUFF.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 28 16:08:41 2018
+
+@author: Aymone
+"""
+
+# TIPS AND STUFF FROM PRACTICAL CODING
+
+# ....to remove variale y; example: del y
+
+# ....to view documentation about a function like in RStudio, press "ctrl+i"
+#i is for inspect.
+
+# .... to put something as a multi-line comment, enclose it with 3 double quotes """blala""""
+
+
